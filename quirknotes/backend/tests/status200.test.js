e47fd4d60e98bf05452dd5d377f7503a317b307e
KaspianTC <kaspian.thoft.christensen@mail.utoronto.ca>
2024-02-16 23:24:06 -0500

    finished getallnotes

--- quirknotes/backend/tests/status200.test.js
+++ quirknotes/backend/tests/status200.test.js
@@ -24,47 +24,95 @@ test("/postNote - Post a note", async () => {
   expect(postNoteRes.status).toBe(200);
   expect(postNoteBody.response).toBe("Note added succesfully.");
 });
+const postNote = async (title, content) => { //helper function
+  const postNoteRes = await fetch(`${SERVER_URL}/postNote`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+      content: content,
+    }),
+  });
+
+  const postNoteBody = await postNoteRes.json();
+  return postNoteBody;
+};
+
 test("/getAllNotes - Return list of zero notes for getAllNotes", async () => {
-  // Code here
-  expect(false).toBe(true);
+  const getAllNoteRes = await fetch(`${SERVER_URL}/getAllNotes`, {
+    method: "GET",
+  });
+  const getAllNoteBody = await getAllNoteRes.json();
+
+  expect(getAllNoteRes.status).toBe(200);
+  expect(getAllNoteBody.response).toStrictEqual([]);
 });
 
 test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
   // Code here
-  expect(false).toBe(true);
+  const getAllNoteRes = await fetch(`${SERVER_URL}/getAllNotes`, {
+    method: "GET",
+  });
+  const getAllNoteBody = await getAllNoteRes.json();
+
+  expect(getAllNoteRes.status).toBe(200);
+  expect(getAllNoteBody.response).toStrictEqual([]);
 });
 
 test("/deleteNote - Delete a note", async () => {
   // Code here
-  expect(false).toBe(true);
+  const postNoteBody = await postNoteRes.json();
+
+  expect(postNoteRes.status).toBe(200);
+  expect(postNoteBody.response).toBe("Note added succesfully.");
 });
 
 test("/patchNote - Patch with content and title", async () => {
   // Code here
-  expect(false).toBe(true);
+  const postNoteBody = await postNoteRes.json();
+
+  expect(postNoteRes.status).toBe(200);
+  expect(postNoteBody.response).toBe("Note added succesfully.");
 });
 
 test("/patchNote - Patch with just title", async () => {
   // Code here
-  expect(false).toBe(true);
+  const postNoteBody = await postNoteRes.json();
+
+  expect(postNoteRes.status).toBe(200);
+  expect(postNoteBody.response).toBe("Note added succesfully.");
 });
 
 test("/patchNote - Patch with just content", async () => {
   // Code here
-  expect(false).toBe(true);
+  const postNoteBody = await postNoteRes.json();
+
+  expect(postNoteRes.status).toBe(200);
+  expect(postNoteBody.response).toBe("Note added succesfully.");
 });
 
 test("/deleteAllNotes - Delete one note", async () => {
   // Code here
-  expect(false).toBe(true);
+  const postNoteBody = await postNoteRes.json();
+
+  expect(postNoteRes.status).toBe(200);
+  expect(postNoteBody.response).toBe("Note added succesfully.");
 });
 
 test("/deleteAllNotes - Delete three notes", async () => {
   // Code here
-  expect(false).toBe(true);
+  const postNoteBody = await postNoteRes.json();
+
+  expect(postNoteRes.status).toBe(200);
+  expect(postNoteBody.response).toBe("Note added succesfully.");
 });
 
 test("/updateNoteColor - Update color of a note to red (#FF0000)", async () => {
   // Code here
-  expect(false).toBe(true);
+  const postNoteBody = await postNoteRes.json();
+
+  expect(postNoteRes.status).toBe(200);
+  expect(postNoteBody.response).toBe("Note added succesfully.");
 });
